98380370d589325a8a4408491a5c73509c64e9d2
feyu87 <sebastian@spannekrebs.com>
2022-11-10 12:29:02 +0100

    [LF8-IT-DD] - Created Navigation and Bodypart Component,Module,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigationpart/navigation.component';
+import { BodypartComponent } from './bodypart/bodypart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    BodypartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bodypart/bodypart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bodypart',
+  templateUrl: './bodypart.component.html',
+  styleUrls: ['./bodypart.component.css']
+})
+export class BodypartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
