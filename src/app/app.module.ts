c62eb6b339b2493f501c0bb25e084de76ec49664
feyu87 <sebastian@spannekrebs.com>
2022-11-10 13:10:46 +0100

    [LF8-IT-DD] - Created Sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigationpart/navigation.component';
 import { BodypartComponent } from './bodypart/bodypart.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { BodypartComponent } from './bodypart/bodypart.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
